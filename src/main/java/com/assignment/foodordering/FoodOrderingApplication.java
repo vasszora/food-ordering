8f29bb1e46ab64b94e14bf67a53d7f53368dfcf7
vasszora <vasszora@gmail.com>
2023-08-29 09:17:47 +0200

    Created the project

---
+++ src/main/java/com/assignment/foodordering/FoodOrderingApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.foodordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderingApplication.class, args);
+	}
+
+}
